dd41060eb0c6098ec1f422e90e28f7a2059c8a15
Danis <ndexx217@gmail.com>
2025-10-08 18:09:10 +0300

    Initial commit

---
+++ src/test/java/DeliveryCardTest.java
@@ -0,0 +1,73 @@
+import com.codeborne.selenide.Condition;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+import utils.DataGenerator;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class DeliveryCardTest {
+
+    private DataGenerator.UserInfo userInfo;
+
+    @BeforeEach
+    void setUp() {
+        userInfo = DataGenerator.Registration.generateUser("ru");
+    }
+
+    @Test
+    void shouldSuccessBookDelivery() {
+        String planDate = DataGenerator.generateDate(4);
+
+        open("http://localhost:9999");
+
+        fillForm(userInfo, planDate);
+
+        $("[data-test-id='success-notification']")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно запланирована на " + planDate));
+    }
+
+    @Test
+    void shouldRescheduleDelivery() {
+        String firstDate = DataGenerator.generateDate(4);
+        String secondDate = DataGenerator.generateDate(7);
+
+        open("http://localhost:9999");
+
+        fillForm(userInfo, firstDate);
+
+        $("[data-test-id='success-notification']")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно запланирована на " + firstDate));
+
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE));
+        $("[data-test-id='date'] input").setValue(secondDate);
+        $$("button").find(Condition.text("Запланировать")).click();
+
+        $("[data-test-id='replan-notification']")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.text("У вас уже запланирована встреча на другую дату. Перепланировать?"));
+
+        $$("button").find(Condition.text("Перепланировать")).click();
+        $("[data-test-id='success-notification']")
+                .should(Condition.visible, Duration.ofSeconds(15))
+                .should(Condition.text("Встреча успешно запланирована на " + secondDate));
+    }
+
+    private void fillForm(DataGenerator.UserInfo info, String date) {
+        $("[data-test-id='city'] input").setValue(info.getCity());
+
+        $("[data-test-id='date'] input")
+                .sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME, Keys.BACK_SPACE));
+        $("[data-test-id='date'] input").setValue(date);
+
+        $("[data-test-id='name'] input").setValue(info.getName());
+        $("[data-test-id='phone'] input").setValue(info.getPhone());
+        $("[data-test-id='agreement']").click();
+        $$("button").find(Condition.text("Запланировать")).click();
+    }
+}
---
+++ src/test/java/utils/DataGenerator.java
@@ -0,0 +1,72 @@
+package utils;
+
+import com.github.javafaker.Faker;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+public class DataGenerator {
+    private DataGenerator() {
+    }
+
+    public static String generateDate(int shift) {
+        LocalDate date = LocalDate.now().plusDays(shift);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        return date.format(formatter);
+    }
+
+    public static String generateCity(Faker faker) {
+        String[] cities = {"Москва", "Санкт-Петербург", "Казань", "Екатеринбург", "Новосибирск",
+                "Краснодар", "Владивосток", "Калининград", "Сочи", "Уфа"};
+        Random random = new Random();
+        return cities[random.nextInt(cities.length)];
+    }
+
+    public static String generateName(Faker faker) {
+        return faker.name().lastName() + " " + faker.name().firstName();
+    }
+
+    public static String generatePhone(Faker faker) {
+        return "+7" + faker.numerify("##########");
+    }
+
+    public static class Registration {
+        private Registration() {
+        }
+
+        public static UserInfo generateUser(String locale) {
+            Faker faker = new Faker(new Locale(locale));
+            return new UserInfo(
+                    generateCity(faker),
+                    generateName(faker),
+                    generatePhone(faker)
+            );
+        }
+    }
+
+    public static class UserInfo {
+        private final String city;
+        private final String name;
+        private final String phone;
+
+        public UserInfo(String city, String name, String phone) {
+            this.city = city;
+            this.name = name;
+            this.phone = phone;
+        }
+
+        public String getCity() {
+            return city;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String getPhone() {
+            return phone;
+        }
+    }
+}
